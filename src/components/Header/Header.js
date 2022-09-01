cbd0cd819e26b37b577013d44a75954ee084a85d
Kayla Lee <kaylalee2326@gmail.com>
2022-09-01 11:10:41 +1000

    Completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import styled from 'styled-components/macro';
-
-import { COLORS, WEIGHTS } from '../../constants';
-import Logo from '../Logo';
-import SuperHeader from '../SuperHeader';
-import MobileMenu from '../MobileMenu';
+import React from "react";
+import styled from "styled-components/macro";
+import Icon from "../Icon";
+import { COLORS, WEIGHTS, QUERIES } from "../../constants";
+import Logo from "../Logo";
+import SuperHeader from "../SuperHeader";
+import MobileMenu from "../MobileMenu";
+import UnstyledButton from "../UnstyledButton";
 
 const Header = () => {
   const [showMobileMenu, setShowMobileMenu] = React.useState(false);
@@ -32,25 +33,68 @@ const Header = () => {
         <Side />
       </MainHeader>
 
-      <MobileMenu
-        isOpen={showMobileMenu}
-        onDismiss={() => setShowMobileMenu(false)}
-      />
+      <MobileHeaderWrapper>
+        <LeftSide>
+          <Logo />
+        </LeftSide>
+        <RightSide>
+          <UnstyledButton>
+            <Icon id="shopping-bag" strokeWidth={1} />
+          </UnstyledButton>
+          <UnstyledButton>
+            <Icon id="search" strokeWidth={1} />
+          </UnstyledButton>
+          <UnstyledButton onClick={() => setShowMobileMenu(true)}>
+            <Icon id="menu" strokeWidth={1} />
+          </UnstyledButton>
+          <MobileMenu
+            isOpen={showMobileMenu}
+            onDismiss={() => setShowMobileMenu(false)}
+            title="Navigation"
+          />
+        </RightSide>
+      </MobileHeaderWrapper>
     </header>
   );
 };
+const MobileHeaderWrapper = styled.div`
+  display: none;
+
+  @media ${QUERIES.phoneAndSmaller} {
+    display: flex;
+    border-top: 4px solid ${COLORS.gray[900]};
+    border-bottom: 1px solid ${COLORS.gray[300]};
+    padding: 16px 20px;
+  }
+`;
+
+const LeftSide = styled.div`
+  flex: 1;
+`;
+
+const RightSide = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+  gap: 24px;
+`;
 
 const MainHeader = styled.div`
   display: flex;
   align-items: baseline;
   padding: 18px 32px;
-  height: 72px;
+  height: 90px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  overflow: auto;
+
+  @media ${QUERIES.phoneAndSmaller} {
+    display: none;
+  }
 `;
 
 const Nav = styled.nav`
   display: flex;
-  gap: 48px;
+  gap: clamp(16px, 3.2vw + 1rem, 48px);
   margin: 0px 48px;
 `;
 
--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -1,37 +1,89 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components/macro';
-import { DialogOverlay, DialogContent } from '@reach/dialog';
+import React from "react";
+import styled from "styled-components/macro";
+import { DialogOverlay, DialogContent } from "@reach/dialog";
 
-import { QUERIES } from '../../constants';
+import { COLORS, QUERIES } from "../../constants";
 
-import UnstyledButton from '../UnstyledButton';
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import UnstyledButton from "../UnstyledButton";
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
 
-const MobileMenu = ({ isOpen, onDismiss }) => {
-  if (!isOpen) {
-    return null;
-  }
-
+const MobileMenu = ({ isOpen, onDismiss, title }) => {
   return (
-    <div>
-      <button onClick={onDismiss}>Dismiss menu</button>
-      <nav>
-        <a href="/sale">Sale</a>
-        <a href="/new">New&nbsp;Releases</a>
-        <a href="/men">Men</a>
-        <a href="/women">Women</a>
-        <a href="/kids">Kids</a>
-        <a href="/collections">Collections</a>
-      </nav>
-      <footer>
-        <a href="/terms">Terms and Conditions</a>
-        <a href="/privacy">Privacy Policy</a>
-        <a href="/contact">Contact Us</a>
-      </footer>
-    </div>
+    <Overlay isOpen={isOpen} onDismiss={onDismiss}>
+      <Content aria-label={title}>
+        <CloseButton onClick={onDismiss}>
+          <Icon id={"close"}></Icon>
+          <VisuallyHidden>Dismiss menu</VisuallyHidden>
+        </CloseButton>
+        <Nav>
+          <NavLink href="/sale">Sale</NavLink>
+          <NavLink href="/new">New&nbsp;Releases</NavLink>
+          <NavLink href="/men">Men</NavLink>
+          <NavLink href="/women">Women</NavLink>
+          <NavLink href="/kids">Kids</NavLink>
+          <NavLink href="/collections">Collections</NavLink>
+        </Nav>
+        <Footer>
+          <FooterLink href="/terms">Terms and Conditions</FooterLink>
+          <FooterLink href="/privacy">Privacy Policy</FooterLink>
+          <FooterLink href="/contact">Contact Us</FooterLink>
+        </Footer>
+      </Content>
+    </Overlay>
   );
 };
+const Overlay = styled(DialogOverlay)``;
 
+const Content = styled(DialogContent)`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  background-color: ${COLORS.white};
+  padding: 32px 10px 32px 32px;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  gap: 20px;
+`;
+
+const Footer = styled.footer`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  justify-content: flex-end;
+  gap: 8px;
+`;
+
+const CloseButton = styled.button`
+  border: none;
+  background-color: ${COLORS.white};
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-start;
+`;
+
+const NavLink = styled.a`
+  font-size: ${18 / 16}rem;
+  color: ${COLORS.gray[900]};
+  text-decoration: none;
+
+  &:first-of-type {
+    color: ${COLORS.secondary};
+  }
+`;
+
+const FooterLink = styled.a`
+  font-size: ${14 / 16}rem;
+  color: ${COLORS.gray[700]};
+  text-decoration: none;
+`;
 export default MobileMenu;
